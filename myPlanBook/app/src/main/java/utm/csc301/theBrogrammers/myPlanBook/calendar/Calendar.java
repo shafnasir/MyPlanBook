89a293282bac63a2c151897eb0268a8c8fe5d18a
shafnasir <shaf.nasir@gmail.com>
2020-02-12 19:52:18 -0500

    THEB-21: Finished the base Calendar view and started a basic add event activity

--- myPlanBook/app/src/main/java/utm/csc301/theBrogrammers/myPlanBook/calendar/Calendar.java
+++ myPlanBook/app/src/main/java/utm/csc301/theBrogrammers/myPlanBook/calendar/Calendar.java
@@ -1,11 +1,15 @@
 package utm.csc301.theBrogrammers.myPlanBook.calendar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import utm.csc301.theBrogrammers.myPlanBook.R;
 
@@ -13,6 +17,7 @@ public class Calendar extends AppCompatActivity {
 
     CalendarView calendarView;
     TextView myCalendarDate;
+    Button addEvent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,16 @@ public class Calendar extends AppCompatActivity {
 
         calendarView = findViewById(R.id.calendarView);
         myCalendarDate = findViewById(R.id.myCalendarDate);
+        addEvent = findViewById(R.id.addEvent);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // back button pressed
+            }
+        });
 
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
@@ -30,7 +45,15 @@ public class Calendar extends AppCompatActivity {
             }
         });
 
-
-
+        addEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAddEvent();
+            }
+        });
+    }
+    public void openAddEvent(){
+        Intent intent = new Intent(this, AddEvent.class);
+        startActivity(intent);
     }
 }

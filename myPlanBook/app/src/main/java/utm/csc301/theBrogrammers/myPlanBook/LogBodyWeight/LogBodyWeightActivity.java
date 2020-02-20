ac238c8fc0e30e110b2831f6945d8caaed847b21
Alexei Blinov <alexei.blinov@mail.utoronto.ca>
2020-02-19 20:03:14 -0500

    [THEB-37] Finish select date for body weight feature

--- myPlanBook/app/src/main/java/utm/csc301/theBrogrammers/myPlanBook/LogBodyWeight/LogBodyWeightActivity.java
+++ myPlanBook/app/src/main/java/utm/csc301/theBrogrammers/myPlanBook/LogBodyWeight/LogBodyWeightActivity.java
@@ -1,7 +1,11 @@
 package utm.csc301.theBrogrammers.myPlanBook.LogBodyWeight;
 
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -16,7 +20,9 @@ public class LogBodyWeightActivity extends AppCompatActivity {
     CalendarView calendarView;
     LinearLayout calendarLayout;
     TextView myCalendarDate;
+    TextView bodyWeightTextView;
     BodyWeightsModel bodyWeightsModel;
+    Button unitsToggleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,78 +49,86 @@ public class LogBodyWeightActivity extends AppCompatActivity {
         String currentDate = String.valueOf(currentDay) + "/" + String.valueOf(currentMonth) + "/" + String.valueOf(currentYear);
         myCalendarDate.setText(currentDate);
 
+        EditText bodyWeightsInputTextField = findViewById(R.id.inputWeightEditText);
+        bodyWeightsInputTextField.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+
         bodyWeightsModel = new BodyWeightsModel();
         String bodyWeight = bodyWeightsModel.getWeight(currentDate);
+
+        bodyWeightTextView = (TextView)findViewById(R.id.bodyWeightTextView);
+
+        if (bodyWeight == null) {
+            bodyWeightTextView.setVisibility(View.INVISIBLE);
+        }
+        else {
+            bodyWeightTextView.setText(bodyWeight);
+        }
 
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
                 String date = dayOfMonth + "/" + (month + 1) + "/" + year;
                 myCalendarDate.setText(date);
-                bodyWeightsModel.getWeight(date);
+                String weight = bodyWeightsModel.getWeight(date);
+                if (weight == null) {
+                    bodyWeightTextView.setVisibility(View.INVISIBLE);
+                }
+                else {
+                    bodyWeightTextView.setText(weight);
+                    bodyWeightTextView.setVisibility(View.VISIBLE);
+                }
             }
         });
 
-        /*Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        LinearLayout weightsScrollLayout = (LinearLayout) findViewById(R.id.weightsScrollLayout);
-        weightsScrollLayout.setBackgroundColor(Color.TRANSPARENT);
-
-        Button enterWeightButton = (Button) findViewById(R.id.enterWeightButton);
+        Button enterWeightButton = (Button) findViewById(R.id.bodyWeightsEnterButton);
         enterWeightButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 EditText inputWeightEditText = (EditText) findViewById(R.id.inputWeightEditText);
-                ToggleButton toggleUnitsButton = (ToggleButton) findViewById(R.id.toggleUnitsButton);
+                Button toggleUnitsButton = (Button) findViewById(R.id.unitsToggleButton);
+
+                if (inputWeightEditText.getText().toString().isEmpty()){
+                    return;
+                }
 
                 double weight = Double.parseDouble(inputWeightEditText.getText().toString());
                 String units = toggleUnitsButton.getText().toString();
 
-                if (weight > 10000) {
+                String date = myCalendarDate.getText().toString();
+
+                if (weight > 10000 || bodyWeightsModel.getWeight(date) != null) {
                     return;
                 }
 
                 if (units.equals("LBS")) {
-                    TextView newBodyWeight = new TextView(LogBodyWeightActivity.this);
-                    String weightInLbsKg = String.format("%.1f", weight)+ " lbs: " + String.format("%.1f", weight*(1/2.20462)) + " kg";
-                    newBodyWeight.setText(weightInLbsKg);
-                    newBodyWeight.setTextColor(Color.parseColor("#000000"));
-                    newBodyWeight.setTextSize(30);
-                    newBodyWeight.setGravity(Gravity.CENTER);
-                    newBodyWeight.setBackgroundColor(Color.parseColor("#B4FC5B"));
-
-                    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                            LinearLayout.LayoutParams.MATCH_PARENT,
-                            LinearLayout.LayoutParams.WRAP_CONTENT);
-                    params.setMargins(10,10,10,10);
-
-                    newBodyWeight.setLayoutParams(params);
-
-                    LinearLayout weightsScrollLayout = (LinearLayout) findViewById(R.id.weightsScrollLayout);
-                    weightsScrollLayout.addView(newBodyWeight);
+                    String weightInLbsKg = String.format("%.1f", weight)+ " LBS: " + String.format("%.1f", weight*(1/2.20462)) + " KG";
+                    bodyWeightsModel.addWeight(date, weightInLbsKg);
+                    bodyWeightTextView.setText(weightInLbsKg);
+                    bodyWeightTextView.setVisibility(View.VISIBLE);
                 }
                 else {
-                    TextView newBodyWeight = new TextView(LogBodyWeightActivity.this);
-                    String weightInLbsKg = String.format("%.1f", weight*(2.20462))+ " lbs: " + String.format("%.1f", weight) + " kg";
-                    newBodyWeight.setText(weightInLbsKg);
-                    newBodyWeight.setTextColor(Color.parseColor("#000000"));
-                    newBodyWeight.setTextSize(30);
-                    newBodyWeight.setGravity(Gravity.CENTER);
-                    newBodyWeight.setBackgroundColor(Color.parseColor("#B4FC5B"));
-
-                    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                            LinearLayout.LayoutParams.MATCH_PARENT,
-                            LinearLayout.LayoutParams.WRAP_CONTENT);
-                    params.setMargins(10,10,10,10);
-
-                    newBodyWeight.setLayoutParams(params);
-
-                    LinearLayout weightsScrollLayout = (LinearLayout) findViewById(R.id.weightsScrollLayout);
-                    weightsScrollLayout.addView(newBodyWeight);
+                    String weightInLbsKg = String.format("%.1f", weight*(2.20462))+ " LBS: " + String.format("%.1f", weight) + " KG";
+                    bodyWeightsModel.addWeight(date, weightInLbsKg);
+                    bodyWeightTextView.setText(weightInLbsKg);
+                    bodyWeightTextView.setVisibility(View.VISIBLE);
                 }
             }
-        });*/
+        });
+
+        unitsToggleButton = (Button) findViewById(R.id.unitsToggleButton);
+        unitsToggleButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                String units = unitsToggleButton.getText().toString();
+
+                if (units.equals("LBS")) {
+                    unitsToggleButton.setText("KG");
+                }
+                else {
+                    unitsToggleButton.setText("LBS");
+                }
+            }
+        });
     }
 
 }

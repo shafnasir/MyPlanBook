0a725a163be0593d54279ae860e9458a3d14877c
brandonjoubran <brandonjoubran@gmail.com>
2020-02-28 18:55:36 -0500

    [THEB-44] Added the use of a database when the user is creating an account, now their information is stored in a database.

--- myPlanBook/app/src/main/java/utm/csc301/theBrogrammers/myPlanBook/ui/login/Signup.java
+++ myPlanBook/app/src/main/java/utm/csc301/theBrogrammers/myPlanBook/ui/login/Signup.java
@@ -1,5 +1,6 @@
 package utm.csc301.theBrogrammers.myPlanBook.ui.login;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,12 +11,18 @@ import android.widget.Button;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 import utm.csc301.theBrogrammers.myPlanBook.R;
 
 public class Signup extends AppCompatActivity {
 
     private String firstName, lastName, username, password, cellphone, email;
     private EditText lastnameInput, usernameInput, passwordInput, firstnameInput, emailInput, cellphoneInput;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +36,7 @@ public class Signup extends AppCompatActivity {
         passwordInput = findViewById(R.id.passwordInput);
         emailInput = findViewById(R.id.emailInput);
         cellphoneInput = findViewById(R.id.cellInput);
+        mAuth = FirebaseAuth.getInstance();
 
         done.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,6 +56,24 @@ public class Signup extends AppCompatActivity {
         password = passwordInput.getText().toString();
         email = emailInput.getText().toString();
         cellphone = cellphoneInput.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    if(firstName.equals("") || lastName.equals("") || username.equals("") || password.equals("") || email.equals("") || cellphone.equals("")){
+                        Toast.makeText(Signup.this, "One or more fields needs to be filled in", Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(Signup.this, "Signup successful", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(Signup.this, LoginActivity.class);
+                        startActivity(intent);
+                    }
+                }else{
+                    Toast.makeText(Signup.this, "Email not valid", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
 
         if(firstName.equals("") || lastName.equals("") || username.equals("") || password.equals("") || email.equals("") || cellphone.equals("")){
             Toast.makeText(Signup.this, "One or more fields needs to be filled in", Toast.LENGTH_SHORT).show();

7aeb94815013eaefa797a3aa9710bfa85d1f1eee
michaeld <michael.c.delisio@gmail.com>
2020-03-12 01:03:28 -0400

    [THEB-60] Adjust GUI framework for ManualLoadTransactions activity

--- myPlanBook/app/src/main/java/utm/csc301/theBrogrammers/myPlanBook/FinancialHub/ManuallyLoadTransactions.java
+++ myPlanBook/app/src/main/java/utm/csc301/theBrogrammers/myPlanBook/FinancialHub/ManuallyLoadTransactions.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import utm.csc301.theBrogrammers.myPlanBook.R;
 
@@ -18,6 +21,8 @@ public class ManuallyLoadTransactions extends AppCompatActivity {
     private ListView listView;
     private EditText cardNumInput, amountInput, category;
     private CalendarView calendar;
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> transactions = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,11 +46,16 @@ public class ManuallyLoadTransactions extends AppCompatActivity {
 //        this.commitTBtn = (Button) findViewById(R.id.commit_transaction);
         // ListView
         this.listView = (ListView) findViewById(R.id.list_view);
+        this.adapter = new ArrayAdapter<String>(ManuallyLoadTransactions.this,
+                android.R.layout.simple_list_item_1, transactions);
+
+        this.listView.setAdapter(adapter);
     }
 
     public void commitDebit(View v){
+        BankTransaction t = null;
         try {
-            createTransaction(true);
+            t = createTransaction(true);
         } catch (Exception e) {
             e.printStackTrace();
             Toast.makeText(ManuallyLoadTransactions.this,
@@ -53,25 +63,47 @@ public class ManuallyLoadTransactions extends AppCompatActivity {
                     Toast.LENGTH_LONG).show();
             return;
         }
+
+        this.transactions.add("Debit: "+Float.toString(t.getAmount()));
+        this.adapter.notifyDataSetChanged();
+
         Toast.makeText(ManuallyLoadTransactions.this,
                 "Queued Transaction",
                 Toast.LENGTH_LONG).show();
+
     }
 
     public void commitCredit(View v){
+        BankTransaction t = null;
         try {
-            createTransaction(false);
+           t =  createTransaction(false);
         } catch (Exception e) {
             e.printStackTrace();
             Toast.makeText(ManuallyLoadTransactions.this,
                     "Queued Transaction",
-                    Toast.LENGTH_LONG).show();
+                    Toast.LENGTH_SHORT).show();
             return;
         }
+
+        this.transactions.add("Credit: "+Float.toString(t.getAmount()));
+        this.adapter.notifyDataSetChanged();
+
+        Toast.makeText(ManuallyLoadTransactions.this,
+                "Queued Transaction",
+                Toast.LENGTH_SHORT).show();
     }
 
     public void commitCollection(View v){
+        Toast.makeText(ManuallyLoadTransactions.this,
+                "Imported " + transactions.size() + " transactions from . Check \"Manage Transactions" +
+                        "\" page to view them.",
+                Toast.LENGTH_LONG).show();
+        clearCollection(v);
+    }
 
+    public void clearCollection(View v){
+        this.transactions.clear();
+        this.adapter.notifyDataSetChanged();
     }
 
     public BankTransaction createTransaction(Boolean isDebit) throws Exception {

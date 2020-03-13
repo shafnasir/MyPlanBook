c1723c5dfdbce98e5a894fb2cf8afd6e227f2fdb
brandonjoubran <brandonjoubran@gmail.com>
2020-03-13 19:42:56 -0400

    [THEB-69] Goals save when you exit the app and reload when re-enter.

--- myPlanBook/app/src/main/java/utm/csc301/theBrogrammers/myPlanBook/FinancialHub/Goal.java
+++ myPlanBook/app/src/main/java/utm/csc301/theBrogrammers/myPlanBook/FinancialHub/Goal.java
@@ -2,16 +2,38 @@ package utm.csc301.theBrogrammers.myPlanBook.FinancialHub;
 
 public class Goal {
 
-    float currently_at;
-    String goal;
+    String goal_name, price, currently_at;
 
     public Goal(){
 
     }
 
-    public Goal(float currently_at, String goal){
-        this.currently_at = currently_at;
-        this.goal = goal;
+    public Goal(String price, String goal_name){
+        this.price = price;
+        this.goal_name = goal_name;
     }
 
+    public String getCurrently_at() {
+        return currently_at;
+    }
+
+    public void setCurrently_at(String currently_at) {
+        this.currently_at = currently_at;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String currently_at) {
+        this.currently_at = currently_at;
+    }
+
+    public String getGoal_name() {
+        return goal_name;
+    }
+
+    public void setGoal_name(String goal_name) {
+        this.goal_name = goal_name;
+    }
 }
--- myPlanBook/app/src/main/java/utm/csc301/theBrogrammers/myPlanBook/FinancialHub/GoalProgress.java
+++ myPlanBook/app/src/main/java/utm/csc301/theBrogrammers/myPlanBook/FinancialHub/GoalProgress.java
@@ -10,9 +10,16 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -23,6 +30,7 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import utm.csc301.theBrogrammers.myPlanBook.R;
 
@@ -31,7 +39,7 @@ public class GoalProgress extends AppCompatActivity {
     private Button enter, change;
     private EditText goalText, priceText, gainText, spendText;
     private TextView progressText;
-    private String goal, price, gain, spend;
+    private String goal_name, price, gain, spend, user_email;
     private float goal_int = 1;
     private float add = 0;
     private float sub = 0;
@@ -40,6 +48,7 @@ public class GoalProgress extends AppCompatActivity {
     float set = 0;
     Drawable bgDrawable;
     DatabaseReference db;
+    FirebaseUser user;
 
 
     @Override
@@ -49,11 +58,53 @@ public class GoalProgress extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //Setting up the progress bar
         bar = findViewById(R.id.progressBar);
         bgDrawable = bar.getProgressDrawable();
         bgDrawable.setColorFilter(Color.GREEN, android.graphics.PorterDuff.Mode.MULTIPLY);
         bar.setProgressDrawable(bgDrawable);
         bar.setMax(100);
+
+        goalText = findViewById(R.id.goalText);
+        priceText = findViewById(R.id.priceText);
+        progressText = findViewById(R.id.progressText);
+
+        //Get User ID, add child to "Goals" to distinguish between users
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        user_email = user.getUid();
+
+        db = FirebaseDatabase.getInstance().getReference().child("Goals").child(user_email);
+
+        db.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if(dataSnapshot.getChildrenCount() == 0){
+                    return;
+                }
+
+                goal_name = dataSnapshot.child("goal_name").getValue().toString();
+                price = dataSnapshot.child("price").getValue().toString();
+                currently_at = Float.parseFloat(dataSnapshot.child("currently_at").getValue().toString());
+                set = Float.parseFloat(dataSnapshot.child("set").getValue().toString());
+                try {
+                    goal_int = Float.parseFloat(price);
+                    update_bar(currently_at, set);
+                    if(goal_name != null && price != null){
+                        priceText.setText(price);
+                        goalText.setText(goal_name);
+                        update_bar(currently_at, set);
+                    }
+                }catch (Exception e){
+                    Toast.makeText(GoalProgress.this, "Please input a number for price", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
         enter = findViewById(R.id.enterButton);
         enter.setOnClickListener(new View.OnClickListener() {
@@ -67,6 +118,8 @@ public class GoalProgress extends AppCompatActivity {
         change.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                spend = "";
+                gain = "";
                 update_progress();
             }
         });
@@ -76,71 +129,131 @@ public class GoalProgress extends AppCompatActivity {
     }
 
     public void add_goal(){
-        goalText = findViewById(R.id.goalText);
-        priceText = findViewById(R.id.priceText);
-        goal = goalText.getText().toString().trim();
-        price = priceText.getText().toString().trim();
-        //db.child("Goal").setValue(goal);
-        //db.child(goal).child("Price").setValue(price);
-        Log.i("price", price);
-        try {
-            goal_int = Float.parseFloat(price);
-            set = (currently_at/goal_int) * 100;
-            Log.i("goal_int", Float.toString(goal_int));
-        }catch(Exception e){
 
+        goal_name = goalText.getText().toString().trim();
+
+        try {
+            goal_int = Float.parseFloat(priceText.getText().toString().trim());
+            price = priceText.getText().toString().trim();
+            if(goal_int < 0){
+                Toast.makeText(GoalProgress.this, "Please input a positive price", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            set = (currently_at/goal_int) * 100;
+            update_database(goal_name, price);
+        }catch(Exception e){
+            Toast.makeText(GoalProgress.this, "Please input a number for price", Toast.LENGTH_SHORT).show();
         }
     }
 
     public void update_progress(){
 
-        Log.i("goal_int 2", Float.toString(goal_int));
-
         spendText = findViewById(R.id.spendText);
         gainText = findViewById(R.id.gainedText);
-        progressText = findViewById(R.id.progressText);
 
 
         if(goal_int >= 0){
-            spend = spendText.getText().toString().trim();
-            gain = gainText.getText().toString().trim();
-            Log.i("spend", spend);
-            Log.i("gain", gain);
-            if(spend != null || !spend.equals("")){
-                sub = Float.parseFloat(spend);
-                //Log.i("sub", Integer.toString(sub));
+
+            if(spendText == null || spendText.getText().toString().equals("")){
+                sub = 0;
+                spend = "0";
+            }else{
+                try{
+                    spend = spendText.getText().toString().trim();
+                    sub = Float.parseFloat(spend);
+                }catch (Exception e){
+                    Toast.makeText(GoalProgress.this, "Please input a number for spend", Toast.LENGTH_SHORT).show();
+                    return;
+                }
             }
-            if(gain != null || !gain.equals("")){
-                add = Float.parseFloat(gain);
-                //Log.i("add", Integer.toString(add));
+            if(gainText == null || gainText.getText().toString().equals("")){
+                add = 0;
+                gain = "0";
+            }else{
+                try{
+                    gain = gainText.getText().toString().trim();
+                    add = Float.parseFloat(gain);
+                }catch (Exception e){
+                    Toast.makeText(GoalProgress.this, "Please input a number for gain", Toast.LENGTH_SHORT).show();
+                    return;
+                }
             }
 
             currently_at += add-sub;
             set = (currently_at/goal_int) * 100;
-            Log.i("current", Float.toString(currently_at));
-            Log.i("add", Float.toString(add));
-            Log.i("sub", Float.toString(sub));
-            Log.i("goal_int", Float.toString(goal_int));
-            Log.i("add-sub", Float.toString((add-sub)/goal_int));
-            Log.i("set", Float.toString(set));
-            Log.i("set", Integer.toString((int)set));
-            if(set > 100){
-                set = 100;
-                currently_at = 100;
-            }else if(currently_at < 0){
-                set = 0;
-                bgDrawable.setColorFilter(Color.RED, android.graphics.PorterDuff.Mode.MULTIPLY);
-                bar.setProgressDrawable(bgDrawable);
-            }else{
-                bgDrawable.setColorFilter(Color.GREEN, android.graphics.PorterDuff.Mode.MULTIPLY);
-                bar.setProgressDrawable(bgDrawable);
-            }
-            //db.child(goal).child("CurrentProgress").setValue(currently_at);
-            progressText.setText("Progress: $" + currently_at + " (" + set + "%)");
-            ObjectAnimator.ofInt(bar, "progress", (int)set).setDuration(300).start();
+
+
+            update_database(goal_name, price, Float.toString(currently_at), Float.toString(set));
+            update_bar(currently_at, set);
+            spend = "";
+            gain = "";
+            spendText = null;
+            gainText = null;
         }else{
-            //Tell user to enter goal
+            Toast.makeText(GoalProgress.this, "Please input a positive price", Toast.LENGTH_SHORT).show();
         }
+
+
+    }
+
+    public void update_bar(float currently_at, float set){
+
+        progressText = findViewById(R.id.progressText);
+        if(set > 100){
+            set = 100;
+            currently_at = 100;
+        }else if(currently_at < 0){
+            set = 0;
+            bgDrawable.setColorFilter(Color.RED, android.graphics.PorterDuff.Mode.MULTIPLY);
+            bar.setProgressDrawable(bgDrawable);
+        }else{
+            bgDrawable.setColorFilter(Color.GREEN, android.graphics.PorterDuff.Mode.MULTIPLY);
+            bar.setProgressDrawable(bgDrawable);
+        }
+
+        progressText.setText("Progress: $" + currently_at + " (" + set + "%)");
+        ObjectAnimator.ofInt(bar, "progress", (int)set).setDuration(300).start();
+
+    }
+
+    public void update_database(String goal_name, String price){
+
+        db.child("goal_name").setValue(goal_name);
+        db.child("price").setValue(price);
+
+        db.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                Float temp_current = Float.parseFloat(dataSnapshot.child("currently_at").getValue().toString());
+                if(temp_current == 0){
+                    db.child("currently_at").setValue("0");
+                    db.child("set").setValue("0");
+                }else{
+                    set = (temp_current/Float.parseFloat(price)) *100;
+                    db.child("set").setValue(set);
+                    update_bar(temp_current, set);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    public void update_database(String goal_name, String price, String currently_at, String set){
+        db.child("goal_name").setValue(goal_name);
+        db.child("price").setValue(price);
+        db.child("currently_at").setValue(currently_at);
+        db.child("set").setValue(set);
+    }
+
+    public void saved_database(){
+
     }
 
 

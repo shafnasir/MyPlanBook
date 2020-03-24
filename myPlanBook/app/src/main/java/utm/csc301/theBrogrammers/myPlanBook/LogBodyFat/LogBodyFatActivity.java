c770612cfee6e4b897a9dfb2fee11e9824a3b8cd
Alexei Blinov <alexei.blinov@mail.utoronto.ca>
2020-03-23 22:14:34 -0400

    [THEB-85] Finish delete body fat entry feature

--- myPlanBook/app/src/main/java/utm/csc301/theBrogrammers/myPlanBook/LogBodyFat/LogBodyFatActivity.java
+++ myPlanBook/app/src/main/java/utm/csc301/theBrogrammers/myPlanBook/LogBodyFat/LogBodyFatActivity.java
@@ -7,6 +7,7 @@ import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -14,6 +15,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.ViewCompat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.Calendar;
 
@@ -26,27 +29,24 @@ public class LogBodyFatActivity extends AppCompatActivity {
     private LinearLayout.LayoutParams params3;
     private int MARGIN_SIZE;
     private int EDIT_MODE_TV_WIDTH = 875;
-
     private LinearLayout scrollViewLayoutLogBodyFat;
-
     private TextView calendarDate;
     private int currentMonth;
     private String currentDate;
-
     private EditText bodyFatInputTextField;
-
     private BodyFatModel bodyFatModel;
-
     private int bodyFatEntryLayoutId;
     private LinearLayout bodyFatEntryLayout;
-
     private int bodyFatEntryTVId;
     private TextView bodyFatEntryTV;
     private int PADDING_SIZE;
-
     private Button enterBodyFatEntryButton;
-
     private CalendarView calendarView;
+    private int bodyFatEntryCheckBoxId;
+    private CheckBox bodyFatEntryCheckBox;
+    private FloatingActionButton editButton;
+    private FloatingActionButton deleteButton;
+    private Boolean editMode = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,10 +59,13 @@ public class LogBodyFatActivity extends AppCompatActivity {
         this.setCurrentDate();
         bodyFatModel = new BodyFatModel();
         this.createBodyFatEntryLayout();
+        this.createBodyFatEntryCheckBox();
         this.createBodyFatEntryTextView();
         this.setBodyFatEntryTextView();
         this.setEnterButtonListener();
         this.setCalendarViewListener();
+        this.setEditFloatingButtonListener();
+        this.setDeleteFloatingButtonListener();
     }
 
     private void assignParams(){
@@ -111,6 +114,19 @@ public class LogBodyFatActivity extends AppCompatActivity {
         scrollViewLayoutLogBodyFat.addView(this.bodyFatEntryLayout, 6);
     }
 
+    private void createBodyFatEntryCheckBox() {
+        this.bodyFatEntryCheckBox = new CheckBox(this);
+        this.bodyFatEntryCheckBox.setId(ViewCompat.generateViewId());
+        this.bodyFatEntryCheckBoxId = this.bodyFatEntryCheckBox.getId();
+        this.bodyFatEntryCheckBox.setVisibility(View.GONE);
+        LinearLayout.LayoutParams checkBoxParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        checkBoxParams.setMargins(MARGIN_SIZE, 0, 0, 0);
+        this.bodyFatEntryCheckBox.setLayoutParams(checkBoxParams);
+        this.bodyFatEntryLayout.addView(this.bodyFatEntryCheckBox, 0);
+    }
+
     private void createBodyFatEntryTextView(){
         this.bodyFatEntryTV = new TextView(this);
         this.bodyFatEntryTV.setBackgroundResource(R.drawable.box_outline_bordered);
@@ -122,7 +138,7 @@ public class LogBodyFatActivity extends AppCompatActivity {
         this.bodyFatEntryTV.setId(ViewCompat.generateViewId());
         this.bodyFatEntryTVId = bodyFatEntryTV.getId();
         this.bodyFatEntryTV.setVisibility(View.GONE);
-        this.bodyFatEntryLayout.addView(this.bodyFatEntryTV, 0);
+        this.bodyFatEntryLayout.addView(this.bodyFatEntryTV, 1);
     }
 
     private void setBodyFatEntryTextView(){
@@ -139,14 +155,18 @@ public class LogBodyFatActivity extends AppCompatActivity {
         enterBodyFatEntryButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                String bodyFat = bodyFatInputTextField.getText().toString() + "%";
-                if (bodyFat.length() > 10) {
-                    return;
-                }
-                bodyFatModel.addBodyFatEntry(currentDate, bodyFat);
-                setBodyFatEntryTextView();
+                addBodyFatEntry();
             }
         });
+    }
+
+    private void addBodyFatEntry(){
+        String bodyFat = bodyFatInputTextField.getText().toString();
+        if (bodyFat.length() > 10 || bodyFat.isEmpty()) {
+            return;
+        }
+        bodyFatModel.addBodyFatEntry(currentDate, bodyFat + "%");
+        setBodyFatEntryTextView();
     }
 
     private void setCalendarViewListener(){
@@ -154,9 +174,9 @@ public class LogBodyFatActivity extends AppCompatActivity {
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                /*if (editMode == true) {
+                if (editMode == true) {
                     setEditModeFeatures();
-                }*/
+                }
                 String date = dayOfMonth + "/" + (month + 1) + "/" + year;
                 calendarDate.setText(date);
                 currentDate = date;
@@ -164,6 +184,69 @@ public class LogBodyFatActivity extends AppCompatActivity {
                 setBodyFatEntryTextView();
             }
         });
+    }
+
+    private void setEditFloatingButtonListener(){
+        editButton = (FloatingActionButton) findViewById(R.id.edit_button_log_body_fat);
+        editButton.setImageResource(R.drawable.edit_pencil);
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!bodyFatEntryTV.getText().toString().isEmpty()) {
+                    setEditModeFeatures();
+                }
+            }
+        });
+    }
+
+    private void setDeleteFloatingButtonListener(){
+        deleteButton = (FloatingActionButton) findViewById(R.id.delete_button_log_body_fat);
+        deleteButton.setImageResource(R.drawable.garbage_can);
+        deleteButton.hide();
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deleteBodyFatEntry();
+            }
+        });
+    }
+
+    private void setEditModeFeatures(){
+        if (editMode == true){
+            deleteButton.hide();
+        }
+        else {
+            deleteButton.show();
+        }
+        int editDrawableId = editMode == true? R.drawable.edit_pencil: R.drawable.ex_cancel;
+        editButton.setImageResource(editDrawableId);
+        editMode = editMode == true? false: true;
+        setCheckBoxVisibility();
+        setBodyFatEntryTVParams();
+    }
+
+    private void setCheckBoxVisibility(){
+        bodyFatEntryCheckBox.setChecked(false);
+        int visibility = editMode == false? View.GONE: View.VISIBLE;
+        bodyFatEntryCheckBox.setVisibility(visibility);
+    }
+
+    private void setBodyFatEntryTVParams(){
+        if (editMode == false) {
+            bodyFatEntryTV.setLayoutParams(params1);
+        }
+        else {
+            bodyFatEntryTV.setLayoutParams(params3);
+        }
+    }
+
+    private void deleteBodyFatEntry(){
+        if (editMode == false || bodyFatEntryTV.getText().toString().isEmpty() || !bodyFatEntryCheckBox.isChecked()) {
+            return;
+        }
+        bodyFatModel.deleteBodyFatEntry(currentDate);
+        setEditModeFeatures();
+        setBodyFatEntryTextView();
     }
 
 }

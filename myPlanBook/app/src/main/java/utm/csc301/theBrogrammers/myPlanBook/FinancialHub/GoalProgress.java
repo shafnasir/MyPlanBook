595f15e7de5e1218ba57bd408288cd0301dd4d33
brandonjoubran <brandonjoubran@gmail.com>
2020-03-24 15:04:47 -0400

    [THEB-88] Standardizing layout of item goals page

--- myPlanBook/app/src/main/java/utm/csc301/theBrogrammers/myPlanBook/FinancialHub/GoalProgress.java
+++ myPlanBook/app/src/main/java/utm/csc301/theBrogrammers/myPlanBook/FinancialHub/GoalProgress.java
@@ -54,7 +54,7 @@ public class GoalProgress extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_goals);
+        setContentView(R.layout.activity_goal_progress);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
